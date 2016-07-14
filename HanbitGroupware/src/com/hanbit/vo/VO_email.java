fc2ad24554ae1a5cc014872940d9695b2569f118
hb1004 <hb1004@hb1004>
2016-07-14 17:55:06 +0900

    dd

--- HanbitGroupware/src/com/hanbit/vo/VO_email.java
+++ HanbitGroupware/src/com/hanbit/vo/VO_email.java
@@ -1,5 +1,54 @@
 package com.hanbit.vo;
 
 public class VO_email {
+	private String sender_addr, email_addr, subject, content, email_file, regdate;
 
+	public String getSender_addr() {
+		return sender_addr;
+	}
+
+	public void setSender_addr(String sender_addr) {
+		this.sender_addr = sender_addr;
+	}
+
+	public String getEmail_addr() {
+		return email_addr;
+	}
+
+	public void setEmail_addr(String email_addr) {
+		this.email_addr = email_addr;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getEmail_file() {
+		return email_file;
+	}
+
+	public void setEmail_file(String email_file) {
+		this.email_file = email_file;
+	}
+
+	public String getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	
 }
